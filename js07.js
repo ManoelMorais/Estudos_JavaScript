a811e7c3c54eb8908a343edc41f322f32ffe81fd
Manoel <999142818m@gmail.com>
2023-04-05 11:20:18 -0300

    add

---
+++ js07.js
@@ -0,0 +1,37 @@
+
+//modo 1
+
+var c = 1 
+while(c <= 6) {
+    console.log('Bom dia')
+    c++
+}
+
+//modo 2
+
+var c = 1 
+while(c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+//modo 3
+
+var c = 1
+do {
+    console.log(`Bom dia, ${c}`)
+    c++
+} while(c <= 3)
+
+//modo 4
+
+for (var c = 1; c <= 10; c++){
+    console.log(`${c}`)
+}
+
+//modo 5 
+console.log('Preparar em')
+for(var c = 1;c <= 3; c++){
+    console.log(c)
+}
+console.log('GO GO GO GO') 
