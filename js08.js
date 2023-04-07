56105c89791871b52a07f3bc7e5caecc693cddf3
Manoel <999142818m@gmail.com>
2023-04-07 11:03:32 -0300

    add

--- js08.js
+++ js08.js
@@ -6,17 +6,17 @@ console.log(num)
 //console.log(`O vetor é composto por ${num})
 //num[3] = 6
 //num.push(10)
-//num.length //atributo, feito para sabser quantas casas tem um array
-//num.sorte() // coloca em ordem crescente
+//num.length atributo, feito para sabser quantas casas tem um array
+//
 //num.indexOf(7) ta na casa 6
 //num.indexOf(2) não encontrou então ele volta no -1
 //
-//
-//num.sort()
-//num.push(9) //tomar cuidado com o push pq dependendo de onde colocalo ele muda no resultado, ordem influencia 
-//
-//num.push(1)
-//num.sort()
+//num.splace() remover valor especifico
+//num.push() add no final. Tomar cuidado com o push pq dependendo de onde colocalo ele muda no resultado, ordem influencia 
+//num.pop() remover do final
+//num.sort() coloca em ordem crescente
+//num.unshift() add no inicio
+//num.shift() remover do inicio
 //
 //console.log(num)
 console.log(`O vetor e composto por ${num.length}`)
@@ -50,3 +50,9 @@ if (pos == -1){
 //Object
 //let amigo = {nome:'manoel', sexo:'m',peso:70.4, engordar(p){}}
 //                                               [function]
+//          Add o simbolo de preço 
+//document.write(`Preço total ${preço. toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}`)
+//
+//console.log(new Intl.NumberFormat('pt-BR').format(precoTotal))
+//
+//
