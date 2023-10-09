0e43b2c5b048de6f9489cc8596eb11ac770713d2
Manoel <999142818m@gmail.com>
2023-10-09 12:25:09 -0300

    Array

---
+++ JavaScript-organizado/Array-a-Fundo/Indices-e-Elementos/index.js
@@ -0,0 +1,5 @@
+let arr = [1,2,3,4,5,6,7, true, "Manoel", "Morais"];
+
+console.table(arr);
+console.log(arr[7]);
+console.log(arr.length);
---
+++ JavaScript-organizado/Array-a-Fundo/O que  é um Array/index.js
@@ -0,0 +1,7 @@
+/*
+    Array
+    0 - lista de dados
+    1 - Conjunto de Dados
+*/
+
+[1,2,3,4,5,6,7, true, "Manoel", { nome: "Manoel Morais"}];
