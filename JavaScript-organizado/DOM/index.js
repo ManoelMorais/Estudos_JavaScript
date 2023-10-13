e9635c1f4d0db7285d6b761685a830652fb3a12b
Manoel <999142818m@gmail.com>
2023-10-13 19:30:58 -0300

    DOM

---
+++ JavaScript-organizado/DOM/index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const title = document.getElementById('title')
+
+title.style.color = 'white'
+title.style.background = 'blue'
+
+console.log(title)
+
+const h2 = document.querySelector('h2')
+const p = document.querySelectorAll('p')
+const idP = document.querySelector('#paragrafo')
+const classP = document.querySelector('.paragrafo')
+
+console.log(h2)
+console.log(p)
+console.log(idP)
+console.log(classP)
