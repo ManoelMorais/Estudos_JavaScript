c4c18f49570c65b06d136739919ff5f0ff1bcc0f
Manoel <999142818m@gmail.com>
2023-12-12 13:01:49 -0300

    Projeto 01

---
+++ Desafios/01--Project/index.js
@@ -0,0 +1,39 @@
+const apiKey = "288e077f4ef940c7467b2a1d30d02473";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+  if (response.status == 404) {
+    document.querySelector(".weather").style.display = "none";
+    document.querySelector(".error").style.display = "block";
+  } else {
+    var data = await response.json();
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "img/sol-e-chova.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "img/sol.png";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "img/chuva.png";
+    }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
